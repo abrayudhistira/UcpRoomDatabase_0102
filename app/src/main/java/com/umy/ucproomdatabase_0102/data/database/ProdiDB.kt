2c31748c072d3ef8fbe4c6282f8a88815a4cc568
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-16 17:43:21 +0700

    Membuat Room Database dan mengimplementasikan dari entitas yang sudah dibuat

--- app/src/main/java/com/umy/ucproomdatabase_0102/data/database/ProdiDB.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/data/database/ProdiDB.kt
@@ -1,11 +1,35 @@
 package com.umy.ucproomdatabase_0102.data.database
 
+import android.content.Context
+import android.provider.CalendarContract.Instances
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.umy.ucproomdatabase_0102.data.dao.DosenDao
+import com.umy.ucproomdatabase_0102.data.dao.MataKuliahDao
 import com.umy.ucproomdatabase_0102.data.entity.Dosen
 import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
 
 @Database(entities = [MataKuliah::class],[Dosen::class], version = 1, exportSchema = false)
-class ProdiDB : RoomDatabase() {
+abstract class ProdiDB : RoomDatabase() {
+
+    abstract fun dosenDao():DosenDao
+    abstract fun matakuliahDao():MataKuliahDao
+
+    companion object {
+        @Volatile
+        private var Instance: ProdiDB? = null
+
+        fun getDatabase(context: Context): ProdiDB {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    ProdiDB::class.java,
+                    "ProdiDB"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 
 }
