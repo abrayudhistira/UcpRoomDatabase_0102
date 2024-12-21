6f0c7f96c0ce8e3bc78ae7afceb3f090feca5ff5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:42:33 +0700

    membuat fungsi MataKuliahEvent

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,6 +1,17 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
+
 class MataKuliahViewModel
+
+fun MataKuliahEvent.toMatakuliahEntity(): MataKuliah = MataKuliah(
+    kd_mk = kd_mk,
+    nama_mk = nama_mk,
+    sks = sks,
+    semester = semester,
+    jenis = jenis,
+    dosen_pengampu = dosen_pengampu
+)
 
 data class MataKuliahEvent(
     val kd_mk: String = "",
