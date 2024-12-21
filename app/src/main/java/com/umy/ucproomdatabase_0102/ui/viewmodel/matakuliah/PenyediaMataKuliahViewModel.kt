63509e78317f599e6d139f9f9253bc3a4435602e
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:40:03 +0700

    get instance

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/PenyediaMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/PenyediaMataKuliahViewModel.kt
@@ -1,8 +1,12 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.umy.ucproomdatabase_0102.ProdiApp
+import com.umy.ucproomdatabase_0102.data.database.ProdiDB
 import com.umy.ucproomdatabase_0102.ui.viewmodel.dosen.ProdiApp
 
 object PenyediaMataKuliahViewModel {
@@ -32,3 +36,6 @@ object PenyediaMataKuliahViewModel {
         }
     }
 }
+
+fun CreationExtras.ProdiApp(): ProdiApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ProdiApp)
