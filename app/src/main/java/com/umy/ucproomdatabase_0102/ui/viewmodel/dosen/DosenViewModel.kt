963e68946a73e995b3baca70f35d2ac0d9f60690
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 19:36:14 +0700

    Membuat view model dosen

---
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/DosenViewModel.kt
@@ -0,0 +1,8 @@
+package com.umy.ucproomdatabase_0102.ui.viewmodel.dosen
+
+import androidx.lifecycle.ViewModel
+import com.umy.ucproomdatabase_0102.repository.RepoDosen
+
+class DosenViewModel (private val repoDosen: RepoDosen) : ViewModel() {
+
+}
