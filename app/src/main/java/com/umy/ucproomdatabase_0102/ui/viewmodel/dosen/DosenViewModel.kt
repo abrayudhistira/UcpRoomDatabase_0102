d5bace562657102038e6e924c1e330f64a7419d5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 19:40:30 +0700

    fungsi untuk menghandle event dosen

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/DosenViewModel.kt
@@ -1,11 +1,18 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.dosen
 
 import androidx.lifecycle.ViewModel
+import com.umy.ucproomdatabase_0102.data.entity.Dosen
 import com.umy.ucproomdatabase_0102.repository.RepoDosen
 
 class DosenViewModel (private val repoDosen: RepoDosen) : ViewModel() {
 
 }
+
+fun DosenEvent.toDosenEntity():Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jeniskelamin = jeniskelamin,
+)
 
 data class DosenEvent(
     val nidn: String = "",
