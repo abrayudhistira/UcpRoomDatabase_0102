9556759e6f6f747dedf19e9c16bc1cdb0f9f5795
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:21:55 +0700

    fungsi update state

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -1,8 +1,42 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
+import com.umy.ucproomdatabase_0102.repository.RepoMataKuliah
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
-class UpdateMataKuliahViewModel {
+class UpdateMataKuliahViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repoMataKuliah: RepoMataKuliah
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MatakuliahUIState())
+        private set
+
+    private val _KdMk: String = checkNotNull(savedStateHandle[]) //kosongkan dahulu
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repoMataKuliah.getDetail(_KdMk)
+                .filterNotNull()
+                .first()
+                .toUiStateMataKuliah()
+        }
+    }
+
+    fun updateState (mataKuliahEvent: MataKuliahEvent) {
+        updateUIState = updateUIState.copy(
+            matakuliahEvent = mataKuliahEvent
+        )
+    }
+
 }
 
 
