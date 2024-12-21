e20224e6181cadbb3e4d05c875a54535e7fad6eb
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 21:20:25 +0700

    finishing code HomeMataKuliahViewModel

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
@@ -1,12 +1,49 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
 import com.umy.ucproomdatabase_0102.repository.RepoMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMataKuliahViewModel (
     private val repoMataKuliah: RepoMataKuliah
 ) : ViewModel() {
+    val homeMataKuliahUiState: StateFlow<HomeMataKuliahUiState> = repoMataKuliah.getAllMataKuliah()
+        .filterNotNull()
+        .map {
+            HomeMataKuliahUiState (
+                listMataKuliah = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeMataKuliahUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeMataKuliahUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeMataKuliahUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class HomeMataKuliahUiState(
