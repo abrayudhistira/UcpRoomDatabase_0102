bddd5d72f0cd83d01cd2ffbe162614fbd64752cb
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:44:05 +0700

    unfinished PengelolaHalaman

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,19 @@
 package com.umy.ucproomdatabase_0102.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+    ){
+
+    }
+}
