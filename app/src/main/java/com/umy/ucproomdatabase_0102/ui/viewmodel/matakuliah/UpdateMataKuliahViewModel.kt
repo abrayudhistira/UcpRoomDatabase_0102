6cf97b4cc22e55d731ae7e51d036ef54e4d52800
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 21:57:18 +0700

    membuat fungsi untuk implementasi ke state Mata Kuliah

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/UpdateMataKuliahViewModel.kt
@@ -1,4 +1,8 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
+
 class UpdateMataKuliahViewModel {
 }
+
+fun MataKuliah.toUiStateMataKuliah () : MatakuliahUIState = MatakuliahUIState()
