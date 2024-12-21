1ebd7d8655d1eb8583185a10c3bd5f69382e332e
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:51:06 +0700

    finishing code pada class MataKuliahViewModel

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -1,8 +1,36 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
+import com.umy.ucproomdatabase_0102.repository.RepoMataKuliah
 
-class MataKuliahViewModel
+class MataKuliahViewModel(private val repoMataKuliah: RepoMataKuliah
+) : ViewModel() {
+    var uiState by mutableStateOf(MatakuliahUIState())
+
+    fun updateState(matakuliahEvent: MataKuliahEvent) {
+        uiState = uiState.copy(
+            matakuliahEvent = matakuliahEvent
+        )
+    }
+
+    fun validateFields() : Boolean {
+        val event = uiState.matakuliahEvent
+        val errorState = FormErrorState(
+            kd_mk = if (event.kd_mk.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama_mk = if (event.nama_mk.isNotEmpty()) null else "Nama tidak boleh kosong",
+            sks = if (event.sks.isNotEmpty()) null else "SKS tidak boleh kosong",
+            semester = if (event.semester.isNotEmpty()) null else "Semester tidak boleh kosong",
+            jenis = if (event.jenis.isNotEmpty()) null else "Jenis tidak boleh kosong",
+            dosen_pengampu = if (event.dosen_pengampu.isNotEmpty()) null else "Dosen tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
 
 data class MatakuliahUIState(
     val matakuliahEvent: MataKuliahEvent = MataKuliahEvent(),
