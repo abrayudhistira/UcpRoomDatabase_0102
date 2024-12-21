b04e53df1b985ed072d23a8bb89f1de4d3341ee5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:53:33 +0700

    membuat fungsi button

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/view/dashboard/DashboardView.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/view/dashboard/DashboardView.kt
@@ -1,5 +1,33 @@
 package com.umy.ucproomdatabase_0102.ui.view.dashboard
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
 
 @Composable
+fun StaticButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
