1017154853700ff8e7b56e4dea5af1e6fd632422
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 21:59:54 +0700

    membuat fungsi untuk implementasi ke state Mata Kuliah Event

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/DetailMataKuliahViewModel.kt
@@ -1,4 +1,17 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
+
 class DetailMataKuliahViewModel {
+}
+
+fun MataKuliah.toDetailMataKuliahEvent(): MataKuliahEvent {
+    return MataKuliahEvent(
+        kd_mk = kd_mk,
+        nama_mk = nama_mk,
+        sks = sks,
+        semester = semester,
+        jenis = jenis,
+        dosen_pengampu = dosen_pengampu
+    )
 }
