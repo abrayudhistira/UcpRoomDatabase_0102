c23c9ab615d0f73749ee204987a72d31c4f93360
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:04:05 +0700

    menambahkan class HomeUiState serta menginisiasinya

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/HomeDosenViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/HomeDosenViewModel.kt
@@ -1,6 +1,50 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.dosen
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.umy.ucproomdatabase_0102.data.entity.Dosen
+import com.umy.ucproomdatabase_0102.repository.RepoDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDosenViewModel (
+    private val repoDosen: RepoDosen
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repoDosen.getAllDosen()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listDosen = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
+}
 
 data class HomeUiState(
     val listDosen: List<Dosen> = listOf(),
