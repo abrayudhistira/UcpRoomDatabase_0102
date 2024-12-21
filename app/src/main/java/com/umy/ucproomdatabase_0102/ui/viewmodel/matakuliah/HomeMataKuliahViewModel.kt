a43db0f060c6f820c3e7076f681756f7e1fb5884
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 21:16:21 +0700

    membuat data class HomeMataKuliahUiState

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/HomeMataKuliahViewModel.kt
@@ -1,9 +1,17 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.ViewModel
+import com.umy.ucproomdatabase_0102.data.entity.MataKuliah
 import com.umy.ucproomdatabase_0102.repository.RepoMataKuliah
 
 class HomeMataKuliahViewModel (
     private val repoMataKuliah: RepoMataKuliah
 ) : ViewModel() {
 }
+
+data class HomeMataKuliahUiState(
+    val listMataKuliah: List<MataKuliah> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
