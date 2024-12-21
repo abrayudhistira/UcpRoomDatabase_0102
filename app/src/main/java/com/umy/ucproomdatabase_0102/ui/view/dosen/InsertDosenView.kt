c7a417645a7b0f2fdc6659af002fb395678e554c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 23:37:40 +0700

    membuat fungsi FormDosen

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/view/dosen/InsertDosenView.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/view/dosen/InsertDosenView.kt
@@ -1,2 +1,115 @@
 package com.umy.ucproomdatabase_0102.ui.view.dosen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.umy.ucproomdatabase_0102.R
+import com.umy.ucproomdatabase_0102.ui.viewmodel.dosen.DosenEvent
+import com.umy.ucproomdatabase_0102.ui.viewmodel.dosen.FormErrorState
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    val jenisKelamin = listOf("Laki-Laki", "Perempuan")
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            )
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama", color = Color.White) },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama", color = Color.White) },
+            shape = RoundedCornerShape(16.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White,
+                cursorColor = Color.White
+            )
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("NIDN", color = Color.White) },
+            isError = errorState.nidn != null,
+            placeholder = { Text("Masukkan NIDN", color = Color.White) },
+            shape = RoundedCornerShape(16.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedTextColor = Color.White,
+                focusedTextColor = Color.White,
+                cursorColor = Color.White
+            )
+        )
+        Text(
+            text = errorState.nidn ?: "",
+            color = Color.Red
+        )
+
+        Text(text = "Jenis Kelamin", color = Color.White)
+        Row(
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            jenisKelamin.forEach { jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = dosenEvent.jeniskelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jenisKelamin = jk))
+                        },
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = Color.White,
+                            unselectedColor = Color.White
+                        )
+                    )
+                    Text(
+                        text = jk,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
