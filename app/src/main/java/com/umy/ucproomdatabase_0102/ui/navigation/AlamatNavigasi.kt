579d53eb8c5027866da7e6191e4ce3ffde82dc91
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:46:36 +0700

    pembuatan route pada AlamatNavigasi

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/navigation/AlamatNavigasi.kt
@@ -1,3 +1,30 @@
 package com.umy.ucproomdatabase_0102.ui.navigation
 
-interface AlamatNavigasi
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiHalamanUtama : AlamatNavigasi {
+    override val route = "dashboard"
+}
+
+object DestinasiDosen : AlamatNavigasi {
+    override val route = "dosen"
+}
+
+
+object DestinasiMataKuliah : AlamatNavigasi {
+    override val route = "matakuliah"
+}
+
+object DestinasiMataKuliahDetail : AlamatNavigasi {
+    override val route = "matakuliahdetail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiMataKuliahUpdate : AlamatNavigasi {
+    override val route = "matakuliahupdate"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
