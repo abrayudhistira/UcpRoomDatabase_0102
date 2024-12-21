6ca90bbd8072b28197162f4d13bea1bb7846635a
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 22:38:19 +0700

    implementasi view model factory

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/PenyediaMataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/PenyediaMataKuliahViewModel.kt
@@ -1,4 +1,34 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.umy.ucproomdatabase_0102.ui.viewmodel.dosen.ProdiApp
+
 object PenyediaMataKuliahViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            MataKuliahViewModel(
+                ProdiApp().containerApp.repoMataKuliah
+            )
+        }
+        initializer {
+            HomeMataKuliahViewModel(
+                ProdiApp().containerApp.repoMataKuliah
+            )
+        }
+        initializer {
+            DetailMataKuliahViewModel(
+                createSavedStateHandle(),
+                ProdiApp().containerApp.repoMataKuliah
+            )
+        }
+        initializer {
+            UpdateMataKuliahViewModel(
+                createSavedStateHandle(),
+                ProdiApp().containerApp.repoMataKuliah
+            )
+        }
+    }
 }
