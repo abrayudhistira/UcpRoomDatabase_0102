febbca9452c5294c283eb29c1a6e993425284462
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 19:42:03 +0700

    fungsi untuk menghandle error dari validasi

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/DosenViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/DosenViewModel.kt
@@ -8,6 +8,16 @@ class DosenViewModel (private val repoDosen: RepoDosen) : ViewModel() {
 
 }
 
+data class FormErrorState(
+    val nidn:String? =null,
+    val nama:String?= null,
+    val jeniskelamiN:String?=null
+){
+    fun isValid():Boolean{
+        return nidn == null && nama == null &&jeniskelamiN== null
+    }
+}
+
 fun DosenEvent.toDosenEntity():Dosen = Dosen(
     nidn = nidn,
     nama = nama,
