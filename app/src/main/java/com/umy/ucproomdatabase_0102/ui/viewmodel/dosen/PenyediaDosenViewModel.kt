2db17edc55fde5d160b25df85b54952978138e8a
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:16:43 +0700

    finishing code PenyediaDosenViewModel

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/dosen/PenyediaDosenViewModel.kt
@@ -1,4 +1,25 @@
 package com.umy.ucproomdatabase_0102.ui.viewmodel.dosen
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.umy.ucproomdatabase_0102.ProdiApp
+
 object PenyediaDosenViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                ProdiApp().containerApp.repoDosen,
+            )
+        }
+        initializer {
+            HomeDosenViewModel(
+                ProdiApp().containerApp.repoDosen,
+            )
+        }
+    }
 }
+
+fun CreationExtras.ProdiApp(): ProdiApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ProdiApp)
