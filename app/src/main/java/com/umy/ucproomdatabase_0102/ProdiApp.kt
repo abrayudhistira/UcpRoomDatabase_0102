f055ff33e3b47b48af06dcc871fddd97f0b3f5f5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:13:59 +0700

    finishing code ProdiApp

--- app/src/main/java/com/umy/ucproomdatabase_0102/ProdiApp.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ProdiApp.kt
@@ -1,5 +1,14 @@
 package com.umy.ucproomdatabase_0102;
 
-class ProdiApp : Application{
-    lateinit var
+import android.app.Application
+import com.umy.ucproomdatabase_0102.dependeciesinjection.ContainerApp
+import com.umy.ucproomdatabase_0102.dependeciesinjection.InterfaceContainerApp
+
+class ProdiApp : Application() {
+    lateinit var containerApp: InterfaceContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerApp(this)
+    }
 }
