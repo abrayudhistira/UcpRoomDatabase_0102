1c5feef028206c3ad2f7b59cc89c8b68c11bede1
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-21 20:40:46 +0700

    membuat data class MataKuliahEvent

--- app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
+++ app/src/main/java/com/umy/ucproomdatabase_0102/ui/viewmodel/matakuliah/MataKuliahViewModel.kt
@@ -3,10 +3,10 @@ package com.umy.ucproomdatabase_0102.ui.viewmodel.matakuliah
 class MataKuliahViewModel
 
 data class MataKuliahEvent(
-    val kode: String = "",
-    val nama: String = "",
+    val kd_mk: String = "",
+    val nama_mk: String = "",
     val sks: String = "",
     val semester: String = "",
     val jenis: String = "",
-    val dosenPengampu: String = ""
+    val dosen_pengampu: String = ""
 )
